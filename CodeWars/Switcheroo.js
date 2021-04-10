e96269edd70c58e31798a97703ca35ea0aa3ebf6
Victoria <vicabellaru@gmail.com>
2021-04-09 23:29:02 -0400

    edited

---
+++ CodeWars/Switcheroo.js
@@ -0,0 +1,2 @@
+function switcheroo(x){ return x.split('').map(el => (el === 'a') ? 'b' : (el === 'b') ? 'a' : el).join(''); } 
+https://www.codewars.com/kata/57f759bb664021a30300007d/train/javascript 
