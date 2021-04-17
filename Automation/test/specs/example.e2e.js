3b7e3ca3a25102889cc5a9531635d26f3aaa706b
Victoria <vicabellaru@gmail.com>
2021-04-17 19:06:17 -0400

    new test

--- Automation/test/specs/example.e2e.js
+++ Automation/test/specs/example.e2e.js
@@ -1,7 +1,36 @@
-const expect = require('chai').
-expect; describe('Main page test suite', () => { it('Test 1', () => 
-{ browser.url('https://demoqa.com/'); const expectedTitle = 'ToolsQA'; 
-const actualTitle = browser.getTitle(); expect(actualTitle).to.equal(expectedTitle); }); 
-it('Test 2', () => { const elements = $$('div.category-cards > div');
- expect(elements.length).to.equal(6); }); // it('Test 3', () => { // }); // it
- ('Test 4', () => { // }); // it('Test 5', () => { // }); }); 
+const expect = require("chai").expect;
+​
+describe("Main page test suite", () => {
+  it("Validate the title of the main page", () => {
+    browser.url("https://demoqa.com/");
+    const expectedTitle = "ToolsQA";
+    const actualTitle = browser.getTitle();
+    expect(actualTitle).to.equal(expectedTitle);
+  });
+  it("Validate the number of tiles on the main page", () => {
+    const elements = $$("div.category-cards > div");
+    expect(elements.length).to.equal(6);
+  });
+  it('Validate the text of footer', () => {
+    const span = $('#app > footer > span');
+    const expectedText = '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.';
+    const actualText = span.getText();
+    expect(actualText).to.equal(expectedText);
+  });
+  it('Validate the names of tiles', () => {
+    const [actualElements, actualForms, actualAlerts, actualWidgets, actualIteractions, 
+        actualBookStore] = $$('h5').map(el => el.getText());
+    expect(actualElements).to.equal('Elements');
+    expect(actualForms).to.equal('Forms');
+    expect(actualAlerts).to.equal('Alerts, Frame & Windows');
+    expect(actualWidgets).to.equal('Widgets');
+    expect(actualIteractions).to.equal('Interactions');
+    expect(actualBookStore).to.equal('Book Store Application');    
+  });
+  it('Click on Elements tile and validate the target URL', () => {
+    const element = $$('.category-cards > div')[0];
+    element.click();
+    const actualURL = browser.getUrl();
+    expect(actualURL).to.equal('https://demoqa.com/elements');
+  });
+});
