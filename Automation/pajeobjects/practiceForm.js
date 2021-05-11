f4f5fce044b6057388a935a793332bc83679195b
Victoria <vicabellaru@gmail.com>
2021-05-10 23:22:39 -0400

    new

--- Automation/pajeobjects/practiceForm.js
+++ Automation/pajeobjects/practiceForm.js
@@ -12,23 +12,26 @@ class PracticeForm {
     return $$("#city > div:nth-of-type(2) > div > div");
   }
    get ddState1(){
-   return $$("#state > div > div.css-1hwfws3 > div.css-1uccc91-singleValue").getText();
+   return $$("#react-select-3-option-0");
    //
    //$("#react-select-3-option-0")[0].getText();
   }
   get ddState2(){
-    return $("#react-select-3-option-0")[1].getText();
+    return $$("#react-select-3-option-1");
   }
    get ddState3(){
-    return $("#react-select-3-option-0")[2].getText();
+    return $$("#react-select-3-option-2");
    }
    get ddState4(){
-    return $("#react-select-3-option-0")[3].getText();
+    return $$("#react-select-3-option-3");
   }
   get ddStatesList1(){
    return [this.ddState1, this.ddState2, this.ddState3,this.ddState4];
 
- }
+}
+get textSuccess(){
+return (this.ddStatesList1).map(el => el.getText());
+}
  get result(){
  return $$("#state > div:nth-of-type(2) > div > div").map(el => el.getText());
 
